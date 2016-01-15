540243fca3a97e10a823906e370cbdeadfb8efd3
haruosuz <haruosuz@yahoo.co.jp>
2016-01-15 15:45:56 +0900

    update README.md scripts/

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,9 +11,9 @@ wget -P data/ -A "*.csv" -r -np -nd -nv http://mmdb.iab.keio.ac.jp/download/
 bash scripts/run_InspectingData.sh
 
 # Running R scripts
-#Rscript --vanilla scripts/my_analysis_reshape2.R
 Rscript --vanilla scripts/my_analysis.R
 mv Rplots.pdf analysis/
+#Rscript --vanilla scripts/my_analysis_reshape2.R
 
 # Print operating system characteristics
 uname -a
